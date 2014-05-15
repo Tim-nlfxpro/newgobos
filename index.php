a7cf23bbe634e8165951bb63d1a57fd0e021bdf2
Michael Stowe <me@mikestowe.com>
2014-05-15 15:38:31 -0700

    Added Bootstrapper
    Didn’t include the bootstrap index.php file initially, adding now

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Constructor
+ * @version 1.0
+ * @author Michael Stowe
+ */
+
+// Set Config Path
+$config_path = 'config.ini';
+
+// Include Base Classes
+require_once('library/foremen/bootstrap.php');
+require_once('library/foremen/basemodel.php');
+
+// Include Base Functions
+require_once('library/foremen/config.php');
+require_once('library/foremen/router.php');
+?>
